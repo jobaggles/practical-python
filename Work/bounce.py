d74f1aae4527f78036ac6a4f9dd248c84b59b0c3
Jo Baggles <tullyhoe@protonmail.com>
2025-09-21 17:02:52 +1000

    completed bounce
    complete bounce

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100.0
+bounce = 1
+
+while bounce <= 10:
+    height = height - 2 / 5 * height
+    height = round(height, 2)
+    print(bounce, " ", height)
+    bounce += 1
