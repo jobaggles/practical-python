08482a0ad79de4be35c7e72b5845fc17a2398da7
Jo Baggles <tullyhoe@protonmail.com>
2025-09-21 18:16:42 +1000

    did 1.7

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,21 @@
 # mortgage.py
 #
 # Exercise 1.7
+# mortgage.py
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+bonus_payment = 1000
+bonus_duration = 12
+
+while principal > 0:
+    if bonus_duration > 0:
+        principal = principal * (1 + rate / 12) - (payment + bonus_payment)
+        total_paid = total_paid + payment + bonus_payment
+        bonus_duration -= 1
+    else:
+        principal = principal * (1 + rate / 12) - (payment)
+        total_paid = total_paid + payment
+print("Total paid", total_paid)
