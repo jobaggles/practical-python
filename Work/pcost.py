261f6eaed9faba593b85fbdae30347ef4d834613
Jo Baggles <tullyhoe@protonmail.com>
2025-09-22 12:56:35 +1000

    did task 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,11 @@
 # pcost.py
 #
 # Exercise 1.27
+portfolio_total = 0
+updated_contents = list
+with open("Data/portfolio.csv", "rt") as f:
+    next(f)  # skip headers
+    for line in f:
+        line = line.split(",")
+        portfolio_total = portfolio_total + (float(line[1]) * float(line[2]))
+    print(portfolio_total)
