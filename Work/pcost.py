09afa8a28cd861c7267979d75f8476ae7db64333
Jo Baggles <tullyhoe@protonmail.com>
2025-09-22 13:37:51 +1000

    did error handling in pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,11 +1,19 @@
 # pcost.py
 #
 # Exercise 1.27
-portfolio_total = 0
-updated_contents = list
-with open("Data/portfolio.csv", "rt") as f:
-    next(f)  # skip headers
-    for line in f:
-        line = line.split(",")
-        portfolio_total = portfolio_total + (float(line[1]) * float(line[2]))
+def portfolio_cost(filename):
+    portfolio_total = 0
+    filepath = "Data/" + filename
+    with open(filepath, "rt") as f:
+        next(f)
+        for line_number, line in enumerate(f, 1):
+            line = line.split(",")
+            try:
+                float(line[1])
+            except ValueError:
+                print(
+                    f"Could not convert line {line_number}, perhaps it has missing values."
+                )
+                continue
+            portfolio_total = portfolio_total + (float(line[1]) * float(line[2]))
     print(portfolio_total)
