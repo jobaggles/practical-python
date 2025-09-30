e47ad55009a7ecd406da06310dd121dece1d52af
Jo Baggles <tullyhoe@protonmail.com>
2025-09-30 13:12:41 +1000

    modified pcost to use tuples

--- Work/pcost.py
+++ Work/pcost.py
@@ -21,11 +21,32 @@ def portfolio_cost(filename):
     return portfolio_total
 
 
+def read_portfolio(filename):
+    list = []
+    portfolio_total = 0
+    with open(filename, "rt") as f:
+        next(f)
+        for line in csv.reader(f):
+            try:
+                t = (line[0], int(line[1]), float(line[2]))
+                list.append(t)
+                portfolio_total += int(line[1]) * float(line[2])
+            except ValueError:
+                print(
+                    f"Could not convert stock {line[0]}, perhaps it has missing values."
+                )
+                continue
+    return list, portfolio_total
+
+
 if len(sys.argv) == 2:
     filename = sys.argv[1]
 else:
     print("Passing default file path Data/portfolio.csv")
     filename = "Data/portfolio.csv"
 
-cost = portfolio_cost(filename)
-print("Total cost:", cost)
+# cost = portfolio_cost(filename)
+# print("Total cost:", cost)
+
+portfoli0, total = read_portfolio(filename)
+print(portfoli0)
